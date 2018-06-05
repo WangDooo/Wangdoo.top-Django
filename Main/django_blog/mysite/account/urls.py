d51dd82e27823c4ab35e70a4780fa7e06bb1ca32
WangDooo <wangbc1993@163.com>
2018-06-05 23:28:17 +0800

    1

--- Main/django_blog/mysite/account/urls.py
+++ Main/django_blog/mysite/account/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 from django.conf import settings
 
+from django.contrib.auth import views as auth_views
+
 app_name = 'account'
 urlpatterns = [
-	path('login/', views.user_login, name='user_login')
+	# path('login/', views.user_login, name='user_login'), # 自定义的登录
+	path('login/', auth_views.login, name='user_login'), # django 内置的登陆
+	path('new-login/', auth_views.login, {"template_name":"account/login.html"}),
+	path('logout/', auth_views.logout, {"template_name":"account/logout.html"}, name='user_logout'),
 ]
