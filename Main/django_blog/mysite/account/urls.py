2478c9ebdef8ef8b713aa612cc23ee0afec3ee87
WangDooo <wangbc1993@163.com>
2018-06-07 23:21:32 +0800

    1

--- Main/django_blog/mysite/account/urls.py
+++ Main/django_blog/mysite/account/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
 	path('new-login/', auth_views.login, {"template_name":"account/login.html"}),
 	path('logout/', auth_views.logout, {"template_name":"account/logout.html"}, name='user_logout'),
 	path('register/', views.register, name='user_register'),
-	path('password-change/', auth_views.password_change, name='password_change'),
+	path('password-change/', auth_views.password_change, {"post_change_redirect":"/account/password-change-done"}, name='password_change'),
 	path('password-change-done', auth_views.password_change_done, name='password_change_done'),
 ]
