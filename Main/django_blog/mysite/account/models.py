7c59911f8968db8346cf85293827bdddd9504c87
WangDooo <wangbc1993@163.com>
2018-07-02 14:13:39 +0800

    1

--- Main/django_blog/mysite/account/models.py
+++ Main/django_blog/mysite/account/models.py
@@ -17,6 +17,9 @@ class UserInfo(models.Model):
 	address = models.CharField(max_length=100, blank=True)
 	aboutme = models.TextField(blank=True)
 
+	def __str__(self):
+		return "user:{}".format(self.user.username)
+
 class UserInfoTwo(models.Model):
 	user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
 	school = models.CharField(max_length=100, blank=True)
