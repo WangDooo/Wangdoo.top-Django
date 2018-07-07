ded75904deaf9fc256cc2b197429273b85233e66
WangDooo <wangbc1993@163.com>
2018-07-07 22:04:27 +0800

    1

--- Main/django_blog/mysite/article/views.py
+++ Main/django_blog/mysite/article/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
 from .models import ArticleColumn
+from .forms import ArticleColumnForm
 
 @login_required(login_url='/account/login/')
+@csrf_exempt
 def acrticle_column(request):
-	columns = ArticleColumn.objects.filter(user=request.user)
-	return render(request, "article/column/article_column.html", {"columns":columns})
+	if request.method == "GET":
+		columns = ArticleColumn.objects.filter(user=request.user)
+		column_form = ArticleColumnForm()
+		return render(request, "article/column/article_column.html", {"columns":columns, "column_form":column_form})
+
+	if request.method == "POST":
+		
