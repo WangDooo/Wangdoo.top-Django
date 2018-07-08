c101e7fca21fea0c085b68340cd37ac085c614e6
WangDooo <wangbc1993@163.com>
2018-07-08 20:25:11 +0800

    1

--- Main/django_blog/mysite/article/urls.py
+++ Main/django_blog/mysite/article/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'article'
 urlpatterns = [
 	path('article-column/', views.acrticle_column, name='article_column'),
+	path('rename-column/', views.rename_article_column, name="rename_article_column"),
+	path('del-column/', views.del_article_column, name="del_article_column"),
 ]
--- Main/django_blog/mysite/article/views.py
+++ Main/django_blog/mysite/article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
 
 from .models import ArticleColumn
 from .forms import ArticleColumnForm
@@ -15,4 +16,36 @@ def acrticle_column(request):
 		return render(request, "article/column/article_column.html", {"columns":columns, "column_form":column_form})
 
 	if request.method == "POST":
-		
+		column_name = request.POST['column']
+		columns = ArticleColumn.objects.filter(user_id=request.user.id, column=column_name)
+		if columns: # 判断栏目名称是否存在 这里有两个条件1.当前用户2.栏目名称
+			return HttpResponse('2')
+		else:
+			ArticleColumn.objects.create(user=request.user, column=column_name)
+			return HttpResponse('1')
+
+@login_required(login_url='/account/login/')
+@require_POST
+@csrf_exempt
+def rename_article_column(request):
+	column_name = request.POST["column_name"]
+	column_id = request.POST['column_id']
+	try:
+		line = ArticleColumn.objects.get(id=column_id) # 根据id查询到要修改的数据 建立实例对象
+		line.column = column_name # 实现重新赋值
+		line.save() # 保存到数据库
+		return HttpResponse("1")
+	except:
+		return HttpResponse('0')
+
+@login_required(login_url='/account/login/')
+@require_POST
+@csrf_exempt
+def del_article_column(request):
+	column_id = request.POST['column_id']
+	try:
+		line = ArticleColumn.objects.get(id=column_id)
+		line.delete()
+		return HttpResponse("1")
+	except:
+		return HttpResponse("2")
