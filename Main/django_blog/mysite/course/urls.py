693a98cded65d5a696f6e0408cff47cf762b1d21
WangDooo <wangbc1993@163.com>
2018-07-16 22:33:14 +0800

    1

--- Main/django_blog/mysite/course/urls.py
+++ Main/django_blog/mysite/course/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import CourseListView
+from .views import CourseListView, ManageCourseListView, CreateCourseView, DeleteCourseView
 
 app_name = 'course'
 urlpatterns = [
 	path('about/', TemplateView.as_view(template_name="course/about.html"), name='about'),
 	path('course-list/', CourseListView.as_view(), name='course_list'),
+	path('manage-course/', ManageCourseListView.as_view(), name='manage_course'),
+	path('create-course/', CreateCourseView.as_view(), name='create_course'),
+	path('delete-course/<int:pk>/', DeleteCourseView.as_view(), name='delete_course'),
 ]
