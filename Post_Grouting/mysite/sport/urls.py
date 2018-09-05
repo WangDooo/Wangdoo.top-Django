6651d84c98ea2b576b67442397208e9311896862
WangDooo <wangbc1993@163.com>
2018-09-05 22:53:16 +0800

    1

--- Post_Grouting/mysite/sport/urls.py
+++ Post_Grouting/mysite/sport/urls.py
@@ -6,6 +6,7 @@ app_name = 'sport'
 urlpatterns = [
 	path('', views.sport, name='sport'),
 	path('sport-post', views.sport_post, name='sport_post'),
+	path('del-card', views.del_card, name='del_card'),
 	path('sport-list', views.sport_list, name='sport_list'),
 	path('sport-list2', views.sport_list2, name='sport_list2'),
 ]
--- Post_Grouting/mysite/sport/views.py
+++ Post_Grouting/mysite/sport/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.http import require_POST
 
 from .models import Punchcard
+import datetime
 
 def sport(request):
 	return render(request, 'sport/sport.html')
@@ -23,12 +25,37 @@ def sport_post(request):
 			return HttpResponse('<h1>上传中有问题！</h1>')
 		return HttpResponseRedirect(reverse('sport:sport_list2'))
 	else:
-		return render(request, 'sport/sport_post.html')
+		today = datetime.datetime.now().strftime("%Y-%m-%d")
+		sports = Punchcard.objects.filter(user=request.user)
+		sport_today = sports.filter(publish_date=today)
+		if len(sport_today):
+			today_card = '1'
+		else:
+			today_card = '2'
+			sport_today = ['1']
+		return render(request, 'sport/sport_post.html', {"sport_today":sport_today[0],"today_card":today_card})
+
+@login_required(login_url='/account/login/')
+@require_POST
+@csrf_exempt
+def del_card(request):
+	card_id = request.POST['card_id']
+	try:
+		card = Punchcard.objects.get(id=card_id)
+		card.delete()
+		return HttpResponse("1")
+	except:
+		return HttpResponse("2")
 
 
+@login_required(login_url='/account/login/')
 def sport_list2(request):
 	sports = Punchcard.objects.filter(user=request.user)
 	return render(request, 'sport/sport_list2.html', {"sports":sports})
 
+
+
+@login_required(login_url='/account/login/')
 def sport_list(request):
 	return render(request, 'sport/sport_list.html')
+
